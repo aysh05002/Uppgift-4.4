a2560482fd9d87c9a9253ff0ecf3841532b05e79
Souzan <Souzan@DESKTOP-VHKB7GI>
2023-03-03 22:58:25 +0200

    Add project files.

---
+++ Uppgift 4.4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        string x = "";
+        do
+        {
+            Console.WriteLine("Välj ett följande alternativ. \n1. Subtrahera ett tal med ett annat" +
+                "\n2. Dividera ett tal med ett annat \n3. Avsluta programmet");
+            x = Console.ReadLine();
+            switch (x)
+            {
+                case "1":
+                    Console.WriteLine("Tal 1");
+                    int tal1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Tal 2");
+                    int tal2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"{tal1} - {tal2} = {tal1 - tal2}\n");
+                    break;
+                case "2":
+                    Console.WriteLine("Tal 1");
+                    double tal3 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Tal 2");
+                    double tal4 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"{tal3} / {tal4} = {tal3 / tal4}\n");
+                    break;
+                case "3":
+                    Console.WriteLine("Programmet avslutas");
+                    break;
+            }
+        } while (x != "3");
+    }
+}
